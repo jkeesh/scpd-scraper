4fdf9194d76e4e00d0a0e80b9447c55f9e87ee58
delgadoj <djoeman84@gmail.com>
2013-05-12 01:45:12 -0700

    updated readme documentation in scrape.py

--- scrape.py
+++ scrape.py
@@ -8,24 +8,16 @@ from bs4 import BeautifulSoup
 
 """
 
-This program downloads scpd videos for a given class in the order
-that they happened as a wmv, then converts them to a mp4. Each time 
-the script is run, it will update to download all of the undownloaded
-videos. 
+See README for general documentation:
 
-This script is modified from the one by Ben Newhouse (https://github.com/newhouseb).
+Dependencies: 
+1. BeautifulSoup for parsing: [sudo easy_install beautifulsoup4] or [http://www.crummy.com/software/BeautifulSoup/](http://www.crummy.com/software/BeautifulSoup/)
+2. Mechanize for emulating a browser: [sudo easy_install mechanize] or [http://wwwsearch.sourceforge.net/mechanize/](http://wwwsearch.sourceforge.net/mechanize/)
+3. mimms for downloading video streams [sudo apt-get install mimms] or using MacPorts for Mac [http://www.macports.org/](http://www.macports.org/)
+4. (For Apple fanbois who don't want .wmv output) Handbrake CLI, for converting to mp4: [http://handbrake.fr/downloads2.php](http://handbrake.fr/downloads2.php)
 
-Unfortunately, there are lots of dependencies to get it up and running
-1. Handbrake CLI, for converting to mp4: http://handbrake.fr/downloads2.php
-2. BeautifulSoup for parsing: http://www.crummy.com/software/BeautifulSoup/
-3. Mechanize for emulating a browser, http://wwwsearch.sourceforge.net/mechanize/
-
-Usage: python scrape.py [Stanford ID] "Interactive Computer Graphics"
-
-The way I use it is to keep a folder of videos, and once I have watched them, move them
-into a subfolder called watched. So it also wont redowload files that are in a subfolder
-called watched.
-
+Usage: 
+    python scrape.py [yourUserName] [optional flags] "Interactive Computer Graphics" "Programming Abstractions" ...
 
 """
 
